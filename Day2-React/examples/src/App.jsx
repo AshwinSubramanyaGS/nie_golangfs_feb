c4bd4be85145fecda5b7b8760bd64095d4a83749
AshwinSubramanyaGS <ashwin.subramanya@gmail.com>
2025-02-11 12:59:10 +0530

    react first app

---
+++ Day2-React/examples/src/App.jsx
@@ -0,0 +1,10 @@
+import Gereetings from "./Greetings";
+
+export default function App(){
+  return(
+    <>
+      <h1><marquee>Welcome to REACT</marquee></h1>
+      <Gereetings/>
+    </>
+  );
+}
---
+++ Day2-React/examples/src/Greetings.jsx
@@ -0,0 +1,8 @@
+function Gereetings(){
+    return(
+        <>
+            <h3>Hi Im greeting child component</h3>
+        </>
+    );
+}
+export default Gereetings;
